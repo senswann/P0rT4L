7a1860d5b881ff6b6e9bac29f2bd285b5ff0bd27
senswann <84702005+senswann@users.noreply.github.com>
2023-01-30 14:34:56 +0100

    début levelDesign + start debug portal

--- Assets/Script/Props/Portal.cs
+++ Assets/Script/Props/Portal.cs
@@ -8,7 +8,7 @@ public class Portal : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" || other.tag == "Energy")
         {
             other.transform.position = portal.position + portal.up;
         }
